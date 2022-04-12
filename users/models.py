2f7f541542b9f210afe7bf7fff81e7a696fa8dbe
PythonDeveloperVipul <vipul.tagline@gmail.com>
2022-04-12 18:11:59 +0530

    app name change

--- users/models.py
+++ users/models.py
@@ -3,8 +3,6 @@ from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import BaseUserManager, AbstractUser
 
 '''Custom User model'''
-
-
 class UserManager(BaseUserManager):
 
     use_in_migrations = True
