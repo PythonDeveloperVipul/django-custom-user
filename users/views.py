2e9d59614a2925dc30b460f47e0050c81f041707
PythonDeveloperVipul <vipul.tagline@gmail.com>
2022-04-22 12:28:18 +0530

    user crud operation changes

--- users/views.py
+++ users/views.py
@@ -1,44 +1,34 @@
 from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView, DeleteView, UpdateView
-from django.views.generic.list import ListView
-
+from django.views import generic
 from .models import User
 
 
-class UserView(ListView):
+class UserView(generic.ListView):
     model = User
     template_name = 'users/user_list.html'
     context_object_name = 'users_data'
 
 
-class UserCreateView(CreateView):
+class UserCreateView(generic.CreateView):
     model = User
-    template_name = 'users/user_create_update.html'
+    template_name = 'users/user_create.html'
     fields = ['first_name', 'last_name', 'email', 'password']
     success_url = reverse_lazy('user_list_view')
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['page_heading'] = 'Add User'
-        return context
 
-
-class UserDeleteView(DeleteView):
+class UserDeleteView(generic.DeleteView):
     model = User
     template_name = 'users/user_delete.html'
     success_url = reverse_lazy('user_list_view')
 
 
-class UserUpdateView(UpdateView):
+class UserUpdateView(generic.UpdateView):
     model = User
-    template_name = 'users/user_create_update.html'
+    template_name = 'users/user_update.html'
     fields = ['first_name', 'last_name', 'email']
     success_url = reverse_lazy('user_list_view')
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['page_heading'] = 'Update User'
-        return context
+ 
 
 
 
