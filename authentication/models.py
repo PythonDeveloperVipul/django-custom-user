7fbda36d87fea537b145d77ed1654b50eec3889f
PythonDeveloperVipul <vipul.tagline@gmail.com>
2022-04-26 15:30:51 +0530

    custom reset password

--- authentication/models.py
+++ authentication/models.py
@@ -5,21 +5,3 @@ from users.models import User
 class ResetPassword(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
     token=models.CharField(max_length=50,null=True,blank=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
